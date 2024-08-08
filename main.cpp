787ad8050b8ae5d887470bfe7479d748cacdde4c
omer-io <omer.jamil@rakurai.io>
2024-08-09 00:26:54 +0500

    updated

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <benchmark/benchmark.h>
 #include <iostream>
 #include <vector> 
+#include <list>
 using namespace std;
 
 uint64_t sum_of_vector_squared(int num_entries){
@@ -18,13 +19,37 @@ uint64_t sum_of_vector_squared(int num_entries){
     return sum;
     
 }
+
+uint64_t sum_of_list_squared(int num_entries){
+    uint64_t sum = 0;
+    list<uint64_t> nums;
+    for (int i = 0; i < num_entries; i++){
+        nums.push_back(i);
+    }
+    
+    list<uint64_t> numsq;
+    for (auto num : nums){
+        numsq.push_back(num * num);
+        sum += num * num;
+    }
+    return sum;
+    
+}
+
 static void BM_sum_of_vector_squared(benchmark::State& state) {
   for (auto _ : state){
     sum_of_vector_squared(10);
   }
 
 }
+static void BM_sum_of_list_squared(benchmark::State& state) {
+  for (auto _ : state){
+    sum_of_list_squared(10);
+  }
 
-BENCHMARK(BM_sum_of_vector_squared); 
+}
+
+BENCHMARK(BM_sum_of_vector_squared);
+BENCHMARK(BM_sum_of_list_squared);
 
 BENCHMARK_MAIN();
