e68e00d4dd02caf5c681a011dbd2497a301692fe
omer-io <omer.jamil@rakurai.io>
2024-08-08 23:55:13 +0500

    updated

--- main.cpp
+++ main.cpp
@@ -1,20 +1,30 @@
 #include <benchmark/benchmark.h>
 #include <iostream>
+#include <vector> 
 using namespace std;
 
-static void BM_StringCreation(benchmark::State& state) {
-  for (auto _ : state)
-    std::string empty_string;
+uint64_t sum_of_vector_squared(int num_entries){
+    uint64_t sum = 0;
+    vector<uint64_t> nums(num_entries);
+    for (int i = 0; i < num_entries; i++){
+        nums[i] = i;
+    }
+    
+    vector<uint64_t> numsq(num_entries);
+    for (int i = 0; i < num_entries; i++){
+        numsq[i] = nums[i] * nums[i];
+        sum += numsq[i];
+    }
+    return sum;
+    
 }
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
+static void BM_sum_of_vector_squared(benchmark::State& state) {
+  for (auto _ : state){
+    sum_of_vector_squared(10);
+  }
 
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
-  for (auto _ : state)
-    std::string copy(x);
 }
-BENCHMARK(BM_StringCopy);
+
+BENCHMARK(BM_sum_of_vector_squared); 
 
 BENCHMARK_MAIN();
